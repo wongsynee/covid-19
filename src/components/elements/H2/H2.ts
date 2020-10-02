2123ca67c7a592dc4bc424be108afb284a9ca226
Syn-Ee Wong <wongsynee@gmail.com>
2020-10-02 18:25:20 +1000

    Added in Strong and H2 typography elements.

---
+++ src/components/elements/H2/H2.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const H2 = styled.h2`
+  font-size: ${({ theme }) => theme.fontSize.md};
+`
+
+export default H2
---
+++ src/components/elements/Strong/Strong.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Strong = styled.strong`
+  font-weight: ${({ theme }) => theme.fontWeight.extraBold};
+`
+
+export default Strong
